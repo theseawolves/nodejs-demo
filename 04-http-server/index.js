49a41ac777d78f5c18ebf2dedd04d07dfed360b5
theseawolves <theseawolves@yeah.net>
2016-09-17 15:52:35 +0800

    add http server

---
+++ 04-http-server/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const port = 3000
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server')
+}
+const server = http.createServer(requestHandler)
+server.listen(port,(err) => {
+  if(err){
+    return console.log(err)
+  }
+  console.log(`server is listening on ${port}`)
+})
